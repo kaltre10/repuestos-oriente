3fbe7bbef199bb25595530b04346d61174dfec2c
manuel perez <manuelperez.0000@gmail.com>
2026-02-01 11:54:43 -0400

    merge de conflictos validateOrderIntegrity

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,24 +1,16 @@
 import express from 'express';
 import { getOrders, getOrder, getOrdersByBuyerId, createOrder, updateOrder, deleteOrder } from '../controllers/order.controller.js';
-<<<<<<< HEAD
 import validateToken from "../midelwares/validateToken.js";
-=======
 import { validateOrderIntegrity } from '../midelwares/validateOrderIntegrity.js';
->>>>>>> f734c489f663a66104ada068085b7bfab9806ee1
 
 const router = express.Router();
 
 router.get('/', getOrders);
 router.get('/:id', getOrder);
 router.get('/buyer/:buyerId', getOrdersByBuyerId);
-<<<<<<< HEAD
-router.post('/',validateToken, createOrder);
+
+router.post('/', validateOrderIntegrity, validateToken, createOrder);
 router.put('/:id',validateToken, updateOrder);
 router.delete('/:id',validateToken, deleteOrder);
-=======
-router.post('/', validateOrderIntegrity, createOrder);
-router.put('/:id', updateOrder);
-router.delete('/:id', deleteOrder);
->>>>>>> f734c489f663a66104ada068085b7bfab9806ee1
 
 export default router;
--- backend/routes/sale.routes.js
+++ backend/routes/sale.routes.js
@@ -1,31 +1,22 @@
 import express from 'express';
 import { uploadReceipt, getSales, getSale, getSalesByUserId, createSale, updateSale, deleteSale, createCheckout, getStats } from '../controllers/sale.controller.js';
 import { uploadMidelware } from '../midelwares/uploadMidelware.js';
-<<<<<<< HEAD
 import validateToken from "../midelwares/validateToken.js";
-=======
 import { validateOrderIntegrity } from '../midelwares/validateOrderIntegrity.js';
-
->>>>>>> f734c489f663a66104ada068085b7bfab9806ee1
 const router = express.Router();
 
 router.get('/', getSales);
 router.get('/stats', getStats);
 router.get('/user/:userId', getSalesByUserId);
 router.get('/:id', getSale);
+router.delete('/:id', validateToken, deleteSale);
+router.put('/:id', validateToken, updateSale);
 
-<<<<<<< HEAD
-router.post('/',validateToken, createSale);
 router.post('/checkout', validateToken, uploadMidelware.single('receiptImage'), createCheckout);
 router.post('/upload-receipt', validateToken, uploadMidelware.single('receiptImage'), uploadReceipt);
-router.put('/:id', validateToken, updateSale);
-router.delete('/:id', validateToken, deleteSale);
-=======
-router.post('/', validateOrderIntegrity, createSale);
-router.post('/checkout', uploadMidelware.single('receiptImage'), validateOrderIntegrity, createCheckout);
-router.post('/upload-receipt', uploadMidelware.single('receiptImage'), uploadReceipt);
-router.put('/:id', updateSale);
-router.delete('/:id', deleteSale);
->>>>>>> f734c489f663a66104ada068085b7bfab9806ee1
+router.post('/', validateOrderIntegrity,validateToken,  createSale);
+
+
+
 
 export default router;
